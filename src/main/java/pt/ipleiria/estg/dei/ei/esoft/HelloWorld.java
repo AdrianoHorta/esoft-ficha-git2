0d17dc8eb2a1879ff25f70a2ec2bd16ddf6e2298
Adriano <2223443@my.ipleiria.pt>
2026-03-10 16:33:29 +0000

    Teste in-app git2

--- src/main/java/pt/ipleiria/estg/dei/ei/esoft/HelloWorld.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld extends JFrame {
     public HelloWorld() {
         setMinimumSize(new Dimension(300, 200));
         setContentPane(painelPrincipal);
-        lblMensagem3.setText("O Horta teve aquiasdsad");
+        lblMensagem3.setText("O Horta teve aquiasdsadsdf");
         setContentPane(lblMensagem3);
         pack();
     }
