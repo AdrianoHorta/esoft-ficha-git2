4606472f32ccb1032cb7a2f33b125cde19d83ff8
Adriano <2223443@my.ipleiria.pt>
2026-03-10 16:13:26 +0000

    adicionar o meu nome

--- src/main/java/pt/ipleiria/estg/dei/ei/esoft/HelloWorld.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/HelloWorld.java
@@ -5,9 +5,12 @@ public class HelloWorld extends JFrame {
 
 
     private JPanel painelPrincipal;
+    private JLabel lblMensagem3;
     public HelloWorld() {
         setMinimumSize(new Dimension(300, 200));
         setContentPane(painelPrincipal);
+        lblMensagem3.setText("O Adriano teve aqui");
+        setContentPane(lblMensagem3);
         pack();
     }
     public static void main(String[] args) {
